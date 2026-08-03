aad17d3670882c046217175494919c12587f35ec
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-03 23:49:14 +0530

    New point : Type convertion

---
+++ Typeconversion.java
@@ -0,0 +1,15 @@
+public class Typeconversion {
+    public static void main(String args[])
+    {
+        //Implicit conversion
+        int no =10;
+        double num = no;
+        System.out.println(num);
+
+        //explicit 
+        double num1 = 102;
+        int no1 = (int) num1;
+        System.out.println(num1);
+
+    }
+}
