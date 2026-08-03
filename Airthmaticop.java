e5e7d4703e344b1690cbc8bac7138d89d75cf968
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-03 23:19:06 +0530

    add new point : airthmatic opration

---
+++ Airthmaticop.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Airthmaticop
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        //Addition
+        System.out.println("Enter Two Numbers :");
+        var a = sc.nextInt();
+        var b = sc.nextInt();
+ //Addition
+        System.out.println("Addition :"+(a+b));
+ //Subtraction
+         System.out.println("Substration :"+(a-b));
+         //Multiplacation
+         System.out.println("Multiplaction :"+(a*b));
+         //Division
+         System.out.println("Division :"+(a/b));
+         
+         
+
+
+
+    }
+}
