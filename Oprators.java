d2273dcda7d9a3f89f870666768fb69911e2756a
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-04 21:08:55 +0530

    add New topic : Oprators

---
+++ Oprators.java
@@ -0,0 +1,38 @@
+public class Oprators 
+{
+    public static void main(String args[])
+    {
+        var a = 10;
+        var b = 20;
+
+        //Airthmatic Oprator
+
+        System.out.println("Add:"+(a+b));
+        System.out.println("Sub:"+(a-b));
+        System.out.println("Mul:"+(a*b));
+        System.out.println("Divide:"+(a/b));
+        System.out.println("Modules:"+(a%b));
+
+        //Realtional Operator
+        System.out.println("Relational oprator");
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a>=b);
+        System.out.println(a<b);
+        System.out.println(a<=b);
+
+        //Logical Operator
+        System.out.println("Logical Operator");
+        System.out.println(10>5 && 20>10);
+        System.out.println(10>20 || 20>5);
+        System.out.println(!(10>20));
+
+       // Assignment Operator
+       System.out.println("Assignment Operator");
+       var c =100;
+       System.out.println(c);
+
+
+    }
+}
