f15281e69d9ab3dfc4e531ccc35e3ea6fef9a514
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-12 00:00:51 +0530

    add new point

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -4,6 +4,7 @@ public class ConditionalStatement
     public static void main(String [] args)
     {
        //IF STATEMENT 
+       System.out.println("IF STATEMENT :");
 
        Scanner sc = new Scanner(System.in);
 
@@ -19,5 +20,22 @@ public class ConditionalStatement
        else{
         System.out.println("B is grether :"+B);
        }
+
+       //EVEN ODD NUMBERS:
+       System.out.println("Enter Numbers:");
+       int NO = sc.nextInt();
+
+       if(NO%2 == 0)
+       {
+        System.out.println("EVEN NO:"+NO);
+       }
+       else{
+        System.out.println("ODD NO:"+NO);
+
+       }
+
+
+
+
     }
 }
