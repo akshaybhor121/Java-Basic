b4ded9bb888df96ade4eeae49fe5070727bdfcf4
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-13 00:42:02 +0530

    While loop

--- Whileloop.java
+++ Whileloop.java
@@ -2,11 +2,12 @@ public class Whileloop
  {
     public static void main(String [] args)
     {
-        int i = 0;
+        int i = 1;
         while(i<=10)
         {
-            System.out.println("Hello Akshay");
+            System.out.println(i);
             i++;
         }
+
     }
 }
