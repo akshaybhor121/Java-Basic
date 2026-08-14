94aed3eb2769793448067a2c77edd324fe4e2d4d
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-14 23:51:40 +0530

    add new topic

--- Forloop.java
+++ Forloop.java
@@ -6,5 +6,12 @@ public class Forloop
         {
             System.out.println("*****");
         }
+
+        
+            for(int j = 0; j<4; j++)
+            {
+                 System.out.println("***");
+            }
+        
     }
 }
