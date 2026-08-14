17af0af1cd4dfa3d5bd5a45a8ca9766138aa1169
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-14 23:55:11 +0530

    add new topic

--- Forloop.java
+++ Forloop.java
@@ -2,17 +2,17 @@ public class Forloop
 {
     public static void main(String [] args)
     {
-        for(int i = 0; i<4; i++)
+        for(int i = 0; i<5; i++)
         {
             System.out.println("*****");
         }
 
         
-            for(int j = 0; j<4; j++)
+            for(int j = 0; j<5; j++)
             {
                  System.out.println("***");
             }
-        for(int k = 0; k<4; k++)
+        for(int k = 0; k<5; k++)
             {
                  System.out.println("*");
             }
