e0fe245856042dde43ea4e747cf7ecdc19d2fe7f
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-15 14:49:49 +0530

    New point

--- Reversenumber.java
+++ Reversenumber.java
@@ -3,13 +3,27 @@ public class Reversenumber
     public static void main(String args [])
     {
         int no = 25633;
+        int l;
         while(no>0)
         {
-        int rev = no % 10;
-        System.out.print(rev);
+         l = no % 10;
+         System.out.print(l);
         no = no/10;
         }
+        
+System.out.println("");
+       System.out.println("OR");
+       //another Logic
+       int n=25366;
+       int rev =0;
+       while(n>0)
+       {
+        int last = n%10;
+        rev = (rev*10)+last;
+        n=n/10;
 
+       }
+       System.out.println(rev);
     }
     
 }
