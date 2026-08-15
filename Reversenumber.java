b38bc870e8a39bb3f7aaceafb2061c47690ec888
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-15 13:09:39 +0530

    New point

--- Reversenumber.java
+++ Reversenumber.java
@@ -5,8 +5,8 @@ public class Reversenumber
         int no = 25633;
         while(no>0)
         {
-        int re = no % 10;
-        System.out.print(re);
+        int rev = no % 10;
+        System.out.print(rev);
         no = no/10;
         }
 
