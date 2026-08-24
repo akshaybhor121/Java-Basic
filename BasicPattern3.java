a3e331cfa326e48a5985fb1574fcf96a52b1f3b2
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-24 19:12:06 +0530

    BasicPattern3 update

--- BasicPattern3.java
+++ BasicPattern3.java
@@ -6,7 +6,7 @@ public class BasicPattern3
         {
             for(int j=1;j<=i;j++)
             {
-                System.out.print(i);
+                System.out.print(j);
             }
             System.out.println(" ");
         }
