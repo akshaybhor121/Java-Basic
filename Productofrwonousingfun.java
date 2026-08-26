6e8552423b5b11cdc4bc77cf55fbbfdd438991ac
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-26 23:57:57 +0530

    Productofrwonousingfun

---
+++ Productofrwonousingfun.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Productofrwonousingfun {
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eneter a two no:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int mul=producte(a,b);
+        System.out.println("Product of two No is :"+mul);
+
+    }
+
+    public static int producte(int a,int b)
+    {
+        int product = a*b;
+        return product;
+    }
+    
+}
