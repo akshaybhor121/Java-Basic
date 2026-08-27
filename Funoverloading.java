d85267370375ea85690083bdfff789a11e2a381f
akshaybhor121 <akshaybhor2006@gmail.com>
2026-08-27 21:29:28 +0530

    Funoverloading

---
+++ Funoverloading.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Funoverloading 
+{
+    public static void main(String [] args)
+    {
+    
+        System.out.println(sum(10,20));
+         System.out.println(sum(10,20,30));
+        
+    }
+    public static int sum(int a,int b)
+    {
+        return a+b;
+    }
+     public static int sum(int a,int b,int c)
+    {
+        return a+b+c;
+    }
+}
